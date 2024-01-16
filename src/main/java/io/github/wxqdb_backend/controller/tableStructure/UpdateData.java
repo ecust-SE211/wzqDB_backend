a23bf3f0d6701e922d8865883f66884a43429bcd
Skuyazlu <celiasash@foxmail.com>
2024-01-16 10:58:56 +0800

    1

--- src/main/java/io/github/wxqdb_backend/controller/tableStructure/UpdateData.java
+++ src/main/java/io/github/wxqdb_backend/controller/tableStructure/UpdateData.java
@@ -4,12 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UpdateData {
+    public String dbName;
     public String tbName;
     public int updataIndex;
-    public Map<String,String> updateKeyValue;
-    public UpdateData()
-    {
-        updataIndex=0;
-        updateKeyValue=new HashMap<String,String>();
+    public Map<String, String> updateKeyValue;
+
+    public UpdateData() {
+        updataIndex = 0;
+        updateKeyValue = new HashMap<String, String>();
     }
 }
